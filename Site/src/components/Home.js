6fc0fb561eb80c2483f5ca6a3a6fded7baf0e7c0
Alexander Ramsey <alexanderjohnramsey@gmail.com>
2018-01-30 12:46:32 +1100

    added home.js

---
+++ Site/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ListItem from './ListItem'
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+      </div>
+    );
+  }
+}
+
+export default Home;
