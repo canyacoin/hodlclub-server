5d0e80247696c49d902901848b00b88e0589f1cb
Tom Nash <contacttomnash@gmail.com>
2018-02-06 10:00:06 +1100

    Add infinite scroll to the members list

--- Site/src/components/Members.js
+++ Site/src/components/Members.js
@@ -3,21 +3,32 @@ import './App.css';
 import ListItem from './ListItem'
 import SearchAddress from './SearchAddress'
 import ListHeader from './ListHeader'
+import InfiniteScroll from 'react-infinite-scroller'
 
 class Members extends React.Component {
   constructor(props) {
     super(props)
-
   }
+
   render() {
     return (<div className="Members">
       <SearchAddress search={this.props.search}/>
       <ListHeader />
-      {
-        this.props.hodlClub.map((hodler, index) => {
-          return (<ListItem hodler={hodler} key={index} />)
-        })
-      }
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={async (page) => {
+          let result = await this.props.loadMore()
+        }}
+        hasMore={this.props.moreToLoad}
+        loader={<div className="loader" key={0}>Loading ...</div>}
+        useWindow={true}
+      >
+        {
+          this.props.hodlClub.map((hodler, index) => {
+            return (<ListItem hodler={hodler} key={index} />)
+          })
+        }
+      </InfiniteScroll>
     </div>);
   }
 }
