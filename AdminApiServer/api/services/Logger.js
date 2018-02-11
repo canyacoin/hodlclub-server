5b3cb1b761c0a65281cc227d84cbb27ac62aab35
Tom Nash <contacttomnash@gmail.com>
2018-02-12 09:53:32 +1100

    Fix path for Admin API logger

--- AdminApiServer/api/services/Logger.js
+++ AdminApiServer/api/services/Logger.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const opts = {
   errorEventName: 'error',
-  logDirectory: path.resolve('./MainApiServer/log'),
+  logDirectory: path.resolve('./AdminApiServer/log'),
   fileNamePattern: 'hodl-<DATE>.log',
   dateFormat: 'YYYY.MM.DD'
 }
