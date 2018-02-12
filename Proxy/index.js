78d2a128c4294bff34ddc314a539b7d29fef297e
Tom Nash <contacttomnash@gmail.com>
2018-02-12 12:11:37 +1100

    Make proxy https

--- Proxy/index.js
+++ Proxy/index.js
@@ -1,13 +1,18 @@
 const httpProxy = require('http-proxy')
-const http = require('http')
+const https = require('https')
+const fs = require('fs')
 
-const proxy = httpProxy.createProxyServer({})
+const proxy = httpProxy.createProxyServer({ ssl: {
+  key: fs.readFileSync(path.resolve('./SSL/wildcard.canya.com.key')),
+  cert: fs.readFileSync(path.resolve('./SSL/wildcard.canya.com_Expires_01Feb2021.cer')),
+  ca: fs.readFileSync(path.resolve('./SSL/wildcard.canya.com.chain'))
+} })
 
 const RequestProxy = {}
 
 RequestProxy.start = (ports) => {
   return new Promise((resolve) => {
-    const server = http.createServer((req, res) => {
+    const server = https.createServer(options, (req, res) => {
       let host = req.headers.host
       if (host.indexOf('api.') === 0) {
         proxy.web(req, res, { target: 'http://localhost:' + ports.api })
