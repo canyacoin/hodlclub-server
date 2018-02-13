2731d077588689d66ad69f40bedc8cb343cf66b3
Alexander Ramsey <alexanderjohnramsey@gmail.com>
2018-02-13 21:06:42 +0300

    Fix merge conflicts

--- Site/src/components/Footer.js
+++ Site/src/components/Footer.js
@@ -4,10 +4,9 @@ class Footer extends React.Component {
   render() {
     return(
       <div className="Footer">
-      <div className="footerContent aligncentre">
-      <a href="www.google.com.au">Built by FLEX Dapps.</a>
-      <p>&nbsp;&nbsp;We build neat, self-contained decentralised applications and smart contracts.</p>
-      </div>
+        <div className="footerContent">
+          <a><a href="www.google.com.au">Built by FLEX Dapps.</a>&nbsp;&nbsp;We build neat, self-contained decentralised applications and smart contracts.</a>
+        </div>
       </div>
     )
   }
